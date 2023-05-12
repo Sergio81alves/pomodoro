8100c9c67aa0bcf114047b77a8a7a54bdd409d89
Sergio Alves <76402373+Sergio81alves@users.noreply.github.com>
2023-05-12 18:39:11 -0300

    Relogio de teste

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-let mensage = "Eu estou nervoso"
-function updateMensage(){
-    let principal = document.getElementById('mensage')
-    principal.innerHTML = mensage
+let digitalEllement = document.querySelector('.digital');
+
+function updateClock() {
+    let now = new Date();
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    let second = now.getSeconds();
+
+    digitalEllement.innerHTML = `${fixZero(hour)}:${fixZero(minute)}:${fixZero(second)}`;
 }
 
-updateMensage();
+function fixZero(time){
+    return time < 10 ? `0${time}` : time;
+ }
+
+setInterval(updateClock,1000);
+updateClock();
