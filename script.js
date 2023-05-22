65e5c6973b3739f55c8db7d74873917ce600baea
Sergio Alves <76402373+Sergio81alves@users.noreply.github.com>
2023-05-22 12:56:47 -0300

    up cronometro

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
 let digitalEllement = document.querySelector('.digital');
+let second = 0
+let minute = 0
+let interval;
 
-function updateClock() {
-    let now = new Date();
-    let hour = now.getHours();
-    let minute = now.getMinutes();
-    let second = now.getSeconds();
-
-    digitalEllement.innerHTML = `${fixZero(hour)}:${fixZero(minute)}:${fixZero(second)}`;
+function start() {
+    interval = setInterval(counter,1000);
 }
+
+function pause(){
+   clearInterval(interval)
+}
+function stopp(){
+    clearInterval(interval)
+    second = 0
+    minute = 0
+    digitalEllement.innerHTML = "00:00"
+}
+
+
+function counter() {
+    second++
+    digitalEllement.innerHTML = `${minute} : ${second}`
+    if(second == 60){
+        minute ++
+        second = 0
+    }
+}
+
 
 function fixZero(time){
     return time < 10 ? `0${time}` : time;
  }
 
-setInterval(updateClock,1000);
-updateClock();
+
+
